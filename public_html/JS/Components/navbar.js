160f07004b4ae5b088b2d03dcd1afabb9215df07
Rendex-sweden <84630659+Rendex-sweden@users.noreply.github.com>
2021-11-11 11:45:59 +0100

    init

--- public_html/JS/Components/navbar.js
+++ public_html/JS/Components/navbar.js
@@ -12,11 +12,17 @@ function navbar(){
             hamburger.setAttribute('aria-label', 'toggled')
             hamburger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" width="40px" viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"/></svg>`
             mobileNav.setAttribute('aria-label', 'toggled')
+            setTimeout(() => {
+                document.getElementById('navbar_logo').setAttribute('src', './Assets/Lash Beauty & Co white.svg')
+            }, 400);
         }
         else{
-            hamburger.setAttribute('aria-label', 'untoggled')
+            setTimeout(() => {
+                hamburger.setAttribute('aria-label', 'untoggled')
+            }, 400);
             hamburger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" width="40px" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"/></svg>`
-            mobileNav.setAttribute('aria-label', 'untoggled') 
+            mobileNav.setAttribute('aria-label', 'untoggled')
+            document.getElementById('navbar_logo').setAttribute('src', './Assets/Lash Beauty & Co.svg')
         }
     }
 }
