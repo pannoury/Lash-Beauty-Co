da62fbd1a099cfc05ce3c0bcfa9ef6de0a265735
Rendex-sweden <84630659+Rendex-sweden@users.noreply.github.com>
2021-11-16 00:19:12 +0100

    minor style fix

---
+++ public_html/PHP/define.php
@@ -0,0 +1,5 @@
+<?php
+    	define( 'INSTAGRAM_APP_ID', '448448153291738' );
+        define( 'INSTAGRAM_APP_SECRET', 'e1ba668cf463c45683124bcadb767009');
+        define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://lashbeautyco.se/' );
+?>
