e7b1a3f5bae07d26e20cbcb7aa2d77e76fdf68a8
Rendex-sweden <84630659+Rendex-sweden@users.noreply.github.com>
2021-12-23 17:43:29 +0100

    Css fix 1.7

--- public_html/JS/Components/navbar.js
+++ public_html/JS/Components/navbar.js
@@ -51,7 +51,7 @@ function loadNavbar(){
     <nav>
         <a id="nav-hamburger" class="hover" aria-label="untoggled"><svg xmlns="http://www.w3.org/2000/svg" height="40px" width="40px" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"/></svg></a>
         <a class="navbar-logo hover" href="https://lashbeautyco.se">
-            <img src="./Assets/logo_v3.svg" id="navbar_logo">
+            <img src="./Assets/logo_v3_alternative.svg" id="navbar_logo">
         </a>
         <div id="navbar-list-desktop">
             <ul>
@@ -83,7 +83,7 @@ function loadFooter(){
         <ul>
             <li class="footer-row">
                 <a href="https://lashbeautyco.se">
-                    <img src="./Assets/logo_v3.svg" id="navbar_logo">
+                    <img src="./Assets/logo_v3_alternative.svg" id="navbar_logo">
                 </a>
                 <div class="footer-row-content">
                     <p class="company-description">
@@ -137,7 +137,7 @@ function loadFooter(){
                         </li>
                         <li>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19.23 15.26l-2.54-.29c-.61-.07-1.21.14-1.64.57l-1.84 1.84c-2.83-1.44-5.15-3.75-6.59-6.59l1.85-1.85c.43-.43.64-1.03.57-1.64l-.29-2.52c-.12-1.01-.97-1.77-1.99-1.77H5.03c-1.13 0-2.07.94-2 2.07.53 8.54 7.36 15.36 15.89 15.89 1.13.07 2.07-.87 2.07-2v-1.73c.01-1.01-.75-1.86-1.76-1.98z"/></svg>
-                            <a href="tel: +46730251600">073-025 16 00</a>
+                            <a href="tel: +46735030455">073-503 04 55</a>
                         </li>
                         <li>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25c-.25-.16-.4-.43-.4-.72 0-.67.73-1.07 1.3-.72L12 11l6.7-4.19c.57-.35 1.3.05 1.3.72 0 .29-.15.56-.4.72z"/></svg>
